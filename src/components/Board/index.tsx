c9fb383a95d0cb6bb649371e505db51b8046e1f9
Lindsay Ferreira <eson@iouu.com.br>
2020-04-16 22:02:47 -0300

    Criada movimentação do heroi

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import Hero from '../Hero'
 import { GAME_SIZE } from '../../setings/constants';
+import MiniDemon from '../MiniDemon';
+import Demon from '../Demon';
+import Chest from '../Chest';
+import Trap from '../Trap';
 
 const Board = () => {
     return(
         <div>
+            <Demon />
+            <MiniDemon />
             <Hero />
+            <Chest />
+            <Trap />
             <img src={'./assets/tileset.gif'} width={GAME_SIZE} height={GAME_SIZE} alt="" />
         </div>
     );
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,21 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { TILE_SIZE } from '../../setings/constants'
+import useEventListener from '@use-it/event-listener'
 
-import  './styles.css';
+import { TILE_SIZE, HEAD_OFSET } from '../../setings/constants'
+
+import './styles.css';
+import { Transform } from 'stream';
+
+const initialPosition = {
+    x: 15,
+    y: 15
+}
 
 const Hero = () => {
+    const [positionState, updatePositionState] = useState(initialPosition);
+    const [direction, updateDirectionState] = useState('RIGHT');
+
+    useEventListener('keydown', (event: any) => {
+        if (event.key === 'ArrowLeft') {
+            updatePositionState({
+                x: positionState.x - 1,
+                y: positionState.y
+            })
+            updateDirectionState('LEFT')
+        } else if (event.key === 'ArrowRight') {
+            updatePositionState({
+                x: positionState.x + 1,
+                y: positionState.y
+            })
+            updateDirectionState('RIGHT')
+
+        } else if (event.key === 'ArrowDown') {
+            updatePositionState({
+                x: positionState.x,
+                y: positionState.y - 1
+            })
+        } else if (event.key === 'ArrowUp') {
+            updatePositionState({
+                x: positionState.x,
+                y: positionState.y + 1
+            })
+
+        }
+    })
+
+
     return (
         <div
             style={{
                 position: 'absolute',
-                bottom: 48*2,
-                left: 48*2,
+                bottom: TILE_SIZE * positionState.y,
+                left: TILE_SIZE * positionState.x,
                 width: TILE_SIZE,
-                height:100,
+                height: TILE_SIZE + HEAD_OFSET,
+                backgroundPosition: `0px -${TILE_SIZE - HEAD_OFSET}px`,
                 backgroundImage: 'url(./assets/HERO.png)',
                 backgroundRepeat: 'no-repeat',
-                animation: 'hero-animation 1s steps(4) infinite'
+                animation: 'hero-animation 1s steps(4) infinite',
+                transform: `scaleX(${direction === 'RIGHT' ? 1 : -1})`
             }}
         />
     );
